7eb5d7a87dc73cf61219cc2181bcf63e105fe22f
kielsonzinn <kielsonzinn@gmail.com>
2023-10-30 09:16:47 -0300

    add one comment by rule type

--- src/review.py
+++ src/review.py
@@ -30,6 +30,11 @@ def __review_by_path(path, path_source, rules):
     ).stdout
 
     comments = []
+    comments_by_type = {
+        "MEMBER_NOT_INITIALIZED_IN_HEADER": [],
+        "MEMBER_PRIVATE_HAS_PREFIX_UNDERLINE": [],
+    }
+    path_internal = path.replace(path_source, "")[1:]
 
     for data_obj in data.split('\n'):
         if data_obj == '':
@@ -45,37 +50,51 @@ def __review_by_path(path, path_source, rules):
 
         atr_name = data_obj['name']
         atr_line = data_obj['line']
-        path_internal = path.replace(path_source, "")[1:]
 
         if 'MEMBER_PRIVATE_HAS_PREFIX_UNDERLINE' in rules and data_obj['access'] == 'private' and \
                 not atr_name.startswith("_"):
-            comment = f"""O atributo `{atr_name}` deve possuir o prefixo `_`, por se tratar de uma variavel private<br>
-Arquivo: {path_internal}<br>
-Linha: {atr_line}"""
-            comments.append(
-                __create_comment(
-                    comment_id=__generate_md5(comment),
-                    comment=comment,
-                    start_line=atr_line,
-                    end_line=atr_line,
-                    path=path_internal,
-                )
-            )
+            member_private_has_prefix_underline = comments_by_type['MEMBER_PRIVATE_HAS_PREFIX_UNDERLINE']
+            member_private_has_prefix_underline.append(f'{atr_name} (Line {atr_line})')
+            comments_by_type['MEMBER_PRIVATE_HAS_PREFIX_UNDERLINE'] = member_private_has_prefix_underline
 
         if 'MEMBER_NOT_INITIALIZED_IN_HEADER' in rules and '=' in data_obj['pattern']:
-            comment = f"""O atributo `{atr_name}` nao deve ser inicializado no header, e sim no constructor<br>
-Arquivo: {path_internal}<br>
-Linha: {atr_line}"""
+            member_not_initialized_in_header = comments_by_type['MEMBER_NOT_INITIALIZED_IN_HEADER']
+            member_not_initialized_in_header.append(f'{atr_name} (Line {atr_line})')
+            comments_by_type['MEMBER_NOT_INITIALIZED_IN_HEADER'] = member_not_initialized_in_header
 
-            comments.append(
-                __create_comment(
-                    comment_id=__generate_md5(comment),
-                    comment=comment,
-                    start_line=atr_line,
-                    end_line=atr_line,
-                    path=path_internal,
-                )
+    member_private_has_prefix_underline = comments_by_type['MEMBER_PRIVATE_HAS_PREFIX_UNDERLINE']
+    if len(member_private_has_prefix_underline) > 0:
+        attrs_append = ', '.join(member_private_has_prefix_underline)
+        comment = f"""Atributos private devem possuir o prefixo `_`<br>
+Atributos: {attrs_append}<br>
+Arquivo: {path_internal}"""
+
+        comments.append(
+            __create_comment(
+                comment_id=__generate_md5(comment),
+                comment=comment,
+                start_line=1,
+                end_line=1,
+                path=path_internal,
             )
+        )
+
+    member_not_initialized_in_header = comments_by_type['MEMBER_NOT_INITIALIZED_IN_HEADER']
+    if len(member_not_initialized_in_header) > 0:
+        attrs_append = ', '.join(member_not_initialized_in_header)
+        comment = f"""Atributos nao devem ser inicializado no header, e sim no constructor<br>
+Atributos: {attrs_append}<br>
+Arquivo: {path_internal}"""
+
+        comments.append(
+            __create_comment(
+                comment_id=__generate_md5(comment),
+                comment=comment,
+                start_line=1,
+                end_line=1,
+                path=path_internal,
+            )
+        )
 
     return comments
 
@@ -88,7 +107,8 @@ def __create_comment(comment_id, comment, path, start_line, end_line):
             "language": 'c++',
             "path": path,
             "startInLine": start_line,
-            "endInLine": end_line
+            "endInLine": end_line,
+            "snipset": False
         }
     }
 
